0ff48df9ea09dbb9c015f9b85855ff89aba4ee42
Pedro Coelho <pedroxaviercoelho@hmail.com>
2022-09-30 15:43:25 -0300

    ....

--- src/pages/Cesta/Components/Topo.js
+++ src/pages/Cesta/Components/Topo.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { Image, Dimensions, StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 import Texto from "../../../Components/Texte";
 import topo from "../../../assets/topo.png";
-
-const width = Dimensions.get("screen").width;
+import { Imagem } from "./styles";
 
 const Topo = ({ titulo }) => {
   return (
     <>
-      <Image source={topo} style={estilos.topo} />
+      <Imagem src={topo} />
       <Texto style={estilos.titulo}> {titulo}</Texto>
     </>
   );
@@ -17,10 +16,6 @@ const Topo = ({ titulo }) => {
 export default Topo;
 
 const estilos = StyleSheet.create({
-  topo: {
-    width: "100%",
-    height: (578 / 756) * width,
-  },
   titulo: {
     width: "100%",
     position: "absolute",
---
+++ src/pages/Cesta/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-height: 567px;
+  max-width: 286px;
+  display: flex;
+  flex-direction: column;
+  border: 2px solid black;
+  border-bottom: none;
+  margin-top: 50px;
+  align-items: center;
+`;
+
+export const Layer = styled.img`
+  position: absolute;
+`;
